4350c1ddc51e77a36e26889332558dbf7d6c0207
Justin Tran <Justin.Tran@winmagic.com>
2025-05-14 14:11:07 -0400

    made change to trigger coderabbit code review

--- obj/Debug/net9.0/JobTracker.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/JobTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764ebaf7a08dd06364aace1ba158f85576b86fc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12f060f07b0720ea9bd2e776903ee6e0b545929")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
