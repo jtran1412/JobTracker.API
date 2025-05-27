5716df18ece704c5d19ec0417e9865f540b96454
Justin Tran <Justin.Tran@winmagic.com>
2025-05-27 09:07:22 -0400

    worked on nightmode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,15 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App';
 import theme from './theme'; 
 import './index.css';
+import NightMode from './components/Nightmode';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '1rem' }}>
+        <NightMode />
+      </div>
       <App />
     </ChakraProvider>
   </React.StrictMode>
